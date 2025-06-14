324d3a1188f30174f1b0825c9be72e2cfead6379
Esse <esse@Es.local>
2025-06-14 13:24:19 -0700

    uncommented wp-chache and hooks

--- potteryshop/static/js/wp-cache.js
+++ potteryshop/static/js/wp-cache.js
@@ -1,12 +1,11 @@
-var _zxcvbnSettings = { src: "https://savoy.nordicmade.com/wp-includes/js/zxcvbn.min.js" }; /*! This file is auto-generated */
-!function () {
-    function t() {
-        var t,
-            e = document.createElement("script");
-        return (e.src = _zxcvbnSettings.src), (e.type = "text/javascript"), (e.async = !0), (t = document.getElementsByTagName("script")[0]).parentNode.insertBefore(e, t);
-    }
-    null != window.attachEvent ? window.attachEvent("onload", t) : window.addEventListener("load", t, !1);
-}.call(this);
+//!function () {
+//    function t() {
+//        var t,
+//            e = document.createElement("script");
+//        return (e.src = _zxcvbnSettings.src), (e.type = "text/javascript"), (e.async = !0), (t = document.getElementsByTagName("script")[0]).parentNode.insertBefore(e, t);
+//    }
+//    null != window.attachEvent ? window.attachEvent("onload", t) : window.addEventListener("load", t, !1);
+//}.call(this);
 (function () {
     var c = document.body.className;
     c = c.replace(/woocommerce-no-js/, "woocommerce-js");
